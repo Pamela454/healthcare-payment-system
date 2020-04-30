5aa4a181651d87d9b55c3444ff96be6d117a223b
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-04-30 16:50:26 -0400

    accounts controller method addition

--- backend/app/channels/api/v1/accounts_controller.rb
+++ backend/app/channels/api/v1/accounts_controller.rb
@@ -1,13 +1,38 @@
 class Api::V1::AccountsController < ApplicationController
   def index
+  	@accounts = Account.all
+  	render json: @accounts
   end
 
   def create
+  	@account = Account.new(account_params)
+  	if @account.save
+  		render json: @account
+  	else
+  		render json: {error: 'Error creating new account'}
+  	end
+  end
+
+  def show
+  	@account = Account.find_by(params[:id])
+  	render json: @account 
   end
 
   def update
+  	@account = Account.find_by(params[:id])
+  	@account.update()
+  	render json: @account 
+    
   end
 
   def destroy
+  	@account = Account.find_by(params[:id])
+  	@account.destroy 
+  end
+
+  private
+
+  def account_params
+  	params.require(:account).permit(:name, :balance, :insurance)
   end
 end
