85b942b504a0e56c6df313a7d6eefa541b43274c
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-05-05 16:03:26 -0400

    install packages redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux' //wrapped in provider so has access to store
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import store- create store from redux 
+//import thunk from redux-thunk
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
