a1c3a1591b88992445b714fe7c642818fdf5df2a
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-05-05 21:07:15 -0400

    addition of account reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,17 @@ import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux' //wrapped in provider so has access to store
+import accountReducer from './reducers/accountReducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
-//store - data stored globally 
+let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
+//containers act more like parent components
+//store - data stored globally. could create in separate file and import. 
 //reducer - what to do with store based on certain actions. returns a new version of the store. 
 
 ReactDOM.render(
-    <Provider store=(store)>
+    <Provider store={store}>
 	<App />
 	</Provider>
 	, document.getElementById('root'));
